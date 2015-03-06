8a77e5ff0328926eb37423214dc9edd771dcba50
Dominic Tarr <dominic.tarr@gmail.com>
2015-03-06 18:19:46 +1300

    tests for the intersection of many arrays

--- test/test.js
+++ test/test.js
@@ -22,3 +22,17 @@ test('compliance', function(t) {
   }
   t.end();
 });
+
+test('multiple sets at once', function (t) {
+
+  t.deepEqual(
+    intersect([
+      [1, 2, 3, 4, 6],
+      [2, 4, 5, 6],
+      [2, 5, 6]
+    ]),
+    [2,6]
+  )
+
+  t.end()
+})
